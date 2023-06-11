3460a4c657d457c57c590425877cdd13df050302
henzoparahua <hnz.2894henzop19@gmail.com>
2023-06-11 18:44:59 -0300

    stuff

--- JS/função/paramsPadrao.js
+++ JS/função/paramsPadrao.js
@@ -18,6 +18,6 @@ console.log(soma2(), soma2(3), soma2(1, 2, 3), soma2(0, 0, 0))
 
  // VALOR PADRÃO DO ES2015
  function soma3(a = 1, b = 1, c = 1){
-    return a+b+c
+    return a+b+c 
  }
  console.log(soma3(), soma3(3), soma3(1, 2, 3), soma3('1', 2, '5'))
--- JS/função/paramsVariaveis.js
+++ JS/função/paramsVariaveis.js
@@ -10,3 +10,4 @@ console.log(soma(1))
 console.log(soma(2.3,1.5,7.2,7,9.3,1.45,7.5,0.65))
 console.log(soma(1.1,2.2,"teste"))
 console.log(soma('a'),'b','c')
+
