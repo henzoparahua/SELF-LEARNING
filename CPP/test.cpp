3d54171e880c86f6ddbdf1c87f84e79840d5f986
henzoparahua <hnz.2894henzop19@gmail.com>
2023-12-12 17:55:41 -0300

    stuff

---
+++ CPP/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+int main(void) {
+    // a classe main é uma excessão no que diz respeito ao 
+    // return ela retorna 0 quando nao mencionamos o return, 
+    // assumindo que deu certo.
+  cout << "hello everynyan how are you fine thank you \noomagaaaaa" << endl;
+  cout << 1;
+}
