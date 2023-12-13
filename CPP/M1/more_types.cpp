0db1ed43c26700389de48e5f4107141ce9ab35f3
henzoparahua <hnz.2894henzop19@gmail.com>
2023-12-13 00:56:03 -0300

    learning cpp

---
+++ CPP/M1/more_types.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main (){
+    // long used for a bigger value than int supports
+    // short is used to determine that we need a value not so big as a integer
+    // unsigned is only used for non-negative values
+
+    short int counter;
+    /*it uses less bits than a standart int*/
+
+    short counter_without_int;
+    // it may be ommited the "integer"
+
+
+    long int ants;
+    /*it will occupy more bits than a normal integer*/
+
+    long ants_without_int;
+    // so it does as the short one.
+
+    unsigned int always_positive_value;
+    // it will never be a negative number.
+
+    unsigned int always_positive_value_without_int;
+    // it can be ommited as usual:
+
+    // we also can mix them together:
+
+    unsigned long int big_number;
+    unsigned long big_number_two;
+    
+    unsigned short int lambs;
+    unsigned short lambs_two;
+
+    /* it is well known that long and short cannot 
+     be used together as it affects the same thing
+     if you try this, you may reconsider your 
+     decision of being a programmer.
+    */
+
+}
