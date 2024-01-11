d6e75144e8b8ad6508ef9712768fe508f7113285
henzoparahua <hnz.2894henzop19@gmail.com>
2024-01-10 23:01:55 -0300

    a wild protected component appears

--- CPP/Cisco/P2M2/defining_subclass.cpp
+++ CPP/Cisco/P2M2/defining_subclass.cpp
@@ -3,6 +3,13 @@ using namespace std;
 // 
 
 class Super {
+// There is also a third acess level called "Protected"
+// the keyword protected means that any component marked with it behaves like a public
+// component when used by any of the subclasses and looks like a private component to
+// the rest of the world.
+    protected:
+        int storageplusfifty;
+
     private:
         int storage;
     public:
@@ -18,7 +25,13 @@ class Super {
 // and private supercless components wont be acessible. It consequently means that you're not allowed
 // to use the latter inside the subclass.
 
-class Sub: public Super { };
+class Sub: public Super { 
+    public:
+    int fiftyormore(){
+        storageplusfifty = get() + 50;
+        return storageplusfifty;
+    }
+};
 // Class SubClass : visibility_specifier SuperClass { };
 
 int main(){
@@ -27,4 +40,6 @@ int main(){
     object.put(100);
     object.put(object.get() + 1);
     cout << object.get() << endl;
+    cout << object.fiftyormore() << endl;
+
 }
