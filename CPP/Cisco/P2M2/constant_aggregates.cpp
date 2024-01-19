6c915185161cfc8ab91aa79dc45699e9535f1b5e
henzoparahua <hnz.2894henzop19@gmail.com>
2024-01-19 15:40:46 -0300

    here it is

--- CPP/Cisco/P2M2/constant_aggregates.cpp
+++ CPP/Cisco/P2M2/constant_aggregates.cpp
@@ -4,22 +4,27 @@ using namespace std;
 //  Aggregates (strucutres and arrays as well as arrays of structures and structures
 //  of arrays) may be declared as const too, although the effect are somewhat different.
 //
-//
+
+int fun(const int n)
+{
+    return n*n;
+}
 
 int main ()
 {
-    const int points[5] = {1, 2, 3, 4, 5 , 6};
+    const int points[5] = {1, 2, 3, 4, 5 };
     const struct { int key; } data = { 10 };
 //  Points and data are read-only variables and you mustn't modify them. 
 
 //  Constant Pointers is allowed although the syntaxz used for 
 //  these declarations may be a bit surprising
 
-    int arr[5] = {1, 2, 3, 4, 5, 6};
+    int arr[5] = {1, 2, 3, 4, 5};
     int * const iptr = arr + 2;
 //  As you notice, the const keyword is placed after the * and before the variable name.
 //  The iptr mustn't be modified. This means we cant do something like --iptr;
 //  The entities pointed to by the const pointer may be modified with no restrictions
 //  The following line will be accepted and succesfully performed.
 *iptr = 0;
+    cout << fun(3) << endl;
 }
